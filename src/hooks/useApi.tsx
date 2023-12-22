1b27d852ba6289897e46ba64cabf914da9c144d7
Md Amir <mdamir1712@gmail.com>
2023-12-22 23:15:52 +0530

    Types Correction Done

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,32 +1,51 @@
-import {useApiContext} from '@/context/ApiContext';
-import axios from 'axios';
-import React from 'react'
+import { useApiContext } from "@/context/ApiContext";
+import axios, { AxiosResponse } from "axios";
+import React from "react";
 
+type ApiResponse<T = any> = {
+  data: T;
+  status?: boolean;
+  message?: string;
+};
 
 const useApi = () => {
-    const {apiData, setApiData, apiError, setApiError, apiLoading, setApiLoading}=useApiContext();
+  const {
+    apiData,
+    setApiData,
+    apiError,
+    setApiError,
+    apiLoading,
+    setApiLoading,
+  } = useApiContext();
 
-    const apiClient=async (url: string,options: any={}) => {
-        setApiLoading(true);
-        try {
-            const response=await axios(url,{
-                ...options,
-                withCredentials:true
-            });
+  const apiClient = async <T = any,>(url: string, options: any = {}) => {
+    setApiLoading(true);
+    try {
+      const response: AxiosResponse<ApiResponse<T>> = await axios(url, {
+        ...options,
+        withCredentials: true,
+      });
 
-            if(response.data) {
-                setApiData(response.data);
-                setApiError('');
-                setApiLoading(false);
-            }
+      if (response.data) {
+        setApiData(response.data);
+        setApiError("");
+        setApiLoading(false);
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        const { status, data } = error.response;
+        setApiError(
+          status >= 400 && data.message ? data.message : "Something went wrong"
+        );
+      } else {
+        setApiError("Something went wrong");
+      }
 
-        } catch (error:unknown) {
-            (error?.response?.status>=400&&error?.response?.data?.message)? setApiError(error.response.data.message):setApiError("Something went wrong");
-            setApiData(null);
-            setApiLoading(false);
-        }
+      setApiData([]);
+      setApiLoading(false);
     }
-    return {apiClient};
-}
+  };
+  return { apiClient };
+};
 
-export default useApi
+export default useApi;
--- src/utils/checkValidParams.ts
+++ src/utils/checkValidParams.ts
@@ -1,15 +1,25 @@
 import {isValidJson} from "./isValidJson";
 
-export default function checkValidParams(formData,jsonData,headerData,paramData,setErrorMessage) {
+type FormData={
+    type: string;
+    url: string;
+};
+
+export default function checkValidParams(
+    formData: FormData,
+    jsonData: string|any,
+    headerData: []|any,
+    paramData: []|any,
+    setErrorMessage: React.Dispatch<React.SetStateAction<string>>
+) {
     if(!formData.url) {
-        setErrorMessage('Invalid url');
+        setErrorMessage("Invalid url");
         return false;
     }
     if(!isValidJson(JSON.stringify(jsonData))) {
-        setErrorMessage('Invalid JSON format');
-        return false
+        setErrorMessage("Invalid JSON format");
+        return false;
     }
-
 
     return true;
 }
@@ -18,16 +28,23 @@ type objectType={
     id?: number;
     key?: string;
     value?: string;
-    check?: boolean
-}
+    check?: boolean;
+};
 
 export function getParamsAndHeaderData(data: objectType[]|[]) {
     return data.reduce((op,obj) => {
-        if(obj.hasOwnProperty('key')&&obj.hasOwnProperty('value')&&obj['check']) {
-            const key=obj['key'];
-            const value=obj['value'];
-            op[key]=value;
+        if(
+            obj.hasOwnProperty("key")&&
+            obj.hasOwnProperty("value")&&
+            obj["check"]
+        ) {
+            const key: string|undefined=obj["key"];
+            const value: string|undefined=obj["value"];
+
+            if(key!==undefined&&value!==undefined) {
+                op[key]=value;
+            }
         }
         return op;
-    },{})
+    },{} as {[key: string]: string});
 }
